6126c8eb718a5ff0ff4640a0e8f88e3dd62183d9
Anzi <149545987+anzi7@users.noreply.github.com>
2023-11-20 17:14:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*----typing animation */
 var typed = new Typed(".typing",{
-    strings:["","Doctoral Researcher", "Data Scientist"],
+    strings:["","Doctoral Researcher", "Data Science Enthusiast"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop:true
